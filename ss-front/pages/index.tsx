95aeef3495624b66b64235297d1041ce38dea086
at8in <at8in@naver.com>
2022-06-04 23:08:53 +0900

    Style: #2 : 기존 파일 Formatting

--- ss-front/pages/index.tsx
+++ ss-front/pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>슬기로운 생활!</h1>
-   </div>
-  )
-}
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">슬기로운 생활!</h1>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
