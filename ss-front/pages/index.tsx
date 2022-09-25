9759fd64f974a83a3e377a53b69f0a789f931a4f
at8in <at8in@naver.com>
2022-09-25 15:23:17 +0900

    Build: #5 : tailwindCSS remove

--- ss-front/pages/index.tsx
+++ ss-front/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
 	return (
 		<div>
-			<h1 className="text-3xl font-bold underline">슬기로운 생활!</h1>
+			<h1>슬기로운 생활!</h1>
 		</div>
 	);
 };
